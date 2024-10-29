30558528f9384194ddbce014f0e94e5ad390627f
JoshuaGataric <gataricjoshua@gmail.com>
2024-10-29 12:04:03 -0700

    Create hobbies.py

---
+++ hobbies.py
@@ -0,0 +1,35 @@
+import streamlit as st
+
+# Hobbies Section
+st.subheader("Hobbies")
+
+# Main hobbies overview
+st.write("""
+My main hobbies include watching sports, playing video games, and going off-roading/hiking.
+""")
+
+# Sports subsection
+st.write("### Sports")
+st.write("""
+Since birth, I've been a fan of the Michigan Wolverines, Pittsburgh Steelers, and Pittsburgh Penguins. I consider myself a diehard fan for each team, watching every game and attending some as well.
+""")
+st.image("Sports1.jpeg") 
+st.image("Sports2.JPEG") 
+st.image("Sports3.jpg") 
+
+# Madden subsection
+st.write("### Madden")
+st.write("""
+Football has always been my favorite sport. Although I never had the chance to play it in real life, I've been fascinated with the video game series "Madden." Playing Madden has let me feel the competitive side of football, and I've even traveled across the country to compete in tournaments. During my undergrad, I also represented San Diego State in the LevelNext Madden National Championship each year.
+""")
+st.image("Madden1.jpg") 
+st.image("Madden2.JPG") 
+
+# Off-Roading/Hiking subsection
+st.write("### Off-Roading/Hiking")
+st.write("""
+Since my parents got a Jeep, off-roading and hiking have become a bonding activity for me and my dad. We've explored breathtaking sights and conquered various trails throughout San Diego. One of my favorite trails is Pio Pico in Jamul, with its many offshoots for exploring!
+""")
+st.image("Outdoors1.JPG") 
+st.image("Outdoors2.jpg") 
+st.image("Outdoors3.jpg") 
